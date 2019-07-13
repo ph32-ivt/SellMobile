b05136cea119e26f59b1be08ccbf22570f5cd8de
PHPminhhai <nm.nguyenminhhai@gmail.com>
2019-07-13 19:56:04 +0700

    Giao diện

---
+++ resources/views/admin/layouts/blocks/nav.blade.php
@@ -0,0 +1,99 @@
+ <nav class="bg-white sidebar sidebar-offcanvas" id="sidebar">
+          <div class="user-info">
+            <img src="images/face.jpg" alt="">
+            <p class="name">Richard V.Welsh</p>
+            <p class="designation">Manager</p>
+            <span class="online"></span>
+          </div>
+          <ul class="nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">
+                <img src="images/icons/1.png" alt="">
+                <span class="menu-title">Tổng quan</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('index-category')}}">
+                <img src="images/icons/2.png" alt="">
+                <span class="menu-title">Thể loại</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/forms/index.html">
+                <img src="images/icons/005-forms.png" alt="">
+                <span class="menu-title">Sản phẩm</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/ui-elements/buttons.html">
+                <img src="images/icons/4.png" alt="">
+                <span class="menu-title">Order</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/tables/index.html">
+                <img src="images/icons/5.png" alt="">
+                <span class="menu-title">Hình ảnh</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/charts/index.html">
+                <img src="images/icons/6.png" alt="">
+                <span class="menu-title">Quản Trị</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/icons/index.html">
+                <img src="images/icons/7.png" alt="">
+                <span class="menu-title">User</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/ui-elements/typography.html">
+                <img src="images/icons/8.png" alt="">
+                <span class="menu-title">Typography</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#sample-pages" aria-expanded="false" aria-controls="sample-pages">
+                <img src="images/icons/9.png" alt="">
+                <span class="menu-title">Lịch sử<i class="fa fa-sort-down"></i></span>
+              </a>
+              <div class="collapse" id="sample-pages">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item">
+                    <a class="nav-link" href="pages/samples/blank_page.html">
+                      Thể loại
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="pages/samples/login.html">
+                      Sảm phẩm
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="pages/samples/register.html">
+                      Thành viên
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="pages/samples/404.html">
+                      User
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="pages/samples/500.html">
+                      500
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <img src="images/icons/10.png" alt="">
+                <span class="menu-title">Settings</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
