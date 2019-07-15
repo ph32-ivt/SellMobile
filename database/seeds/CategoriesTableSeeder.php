28ef9d76c9b6c0a7a2c01f36620b2e5731d63074
Nguyen Hoang Kha <hoangkha0164@gmail.com>
2019-07-15 18:28:27 +0700

    data

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -13,23 +13,23 @@ class CategoriesTableSeeder extends Seeder
     {
         DB::table('categories')->insert([
             /*phone*/
-        		['name'=>'Điện thoại di động','cate_slug'=>'dien-thoai-di-dong'],
-                ['name'=>'Samsung','cate_slug'=>'sam-sung'],
-                ['name'=>'Iphone', 'cate_slug'=>'iphone'],
-                ['name'=>'Oppo', 'cate_slug'=>'oppo'],
-                ['name'=>'Xiaomi', 'cate_slug'=>'xiaomi'],
-                ['name'=>'Huawai', 'cate_slug'=>'huawai'],
-                ['name'=>'Nokia', 'cate_slug'=>'nokia'],
-                ['name'=>'Mobile', 'cate_slug'=>'mobile'],
+        		['name'=>'Điện thoại di động','parent_id'=>'0','cate_slug'=>'dien-thoai-di-dong'],
+                ['name'=>'Samsung','parent_id'=>'1','cate_slug'=>'sam-sung'],
+                ['name'=>'Iphone','parent_id'=>'1', 'cate_slug'=>'iphone'],
+                ['name'=>'Oppo','parent_id'=>'1', 'cate_slug'=>'oppo'],
+                ['name'=>'Xiaomi','parent_id'=>'1', 'cate_slug'=>'xiaomi'],
+                ['name'=>'Huawai','parent_id'=>'1', 'cate_slug'=>'huawai'],
+                ['name'=>'Nokia','parent_id'=>'1', 'cate_slug'=>'nokia'],
+                ['name'=>'Mobile','parent_id'=>'1', 'cate_slug'=>'mobile']
                 /*
                 */
 
-        		['name'=>'Máy tính bảng','cate_slug'=>'may-tinh-bang'],
-                /*
-                */
-        		['name'=>'Ốp lưng','cate_slug'=>'op-lung'],
-                ['name'=>'Ốp lưng da','cate_slug'=>'op-lung-da'],
-                ['name'=>'Ốp lưng vải','cate_slug'=>'op-lung-vai']
+        		// ['name'=>'Máy tính bảng','cate_slug'=>'may-tinh-bang'],
+          //       /*
+          //       */
+        		// ['name'=>'Ốp lưng','cate_slug'=>'op-lung'],
+          //       ['name'=>'Ốp lưng da','cate_slug'=>'op-lung-da'],
+          //       ['name'=>'Ốp lưng vải','cate_slug'=>'op-lung-vai']
         ]);
     }
 }
